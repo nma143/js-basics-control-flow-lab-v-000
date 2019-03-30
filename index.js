93c91ac166745e280714cbeaa30995d7c2e2ce03
Nikki A <nikki.apostolakis@gmail.com>
2019-03-30 19:05:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Write your code in this file!
-Function scuberGreetingForFeet(distance) 
+Function scuberGreetingForFeet(distance)
 {
   if distance <= 400
   {
       "This one is on me!"
   }
-  else 
+  else
   {
-    
+
   }
 
-  
+
 }
