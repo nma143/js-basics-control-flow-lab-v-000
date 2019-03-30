7525d80c9fbd14f98640eb39ef14d64dbda39ab1
Nikki A <nikki.apostolakis@gmail.com>
2019-03-30 19:23:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance)
+{
+  if (distance <= 400)
+  {
+    return "This one is on me!";
+  }
+  else if (distance > 2500)
+  {
+    return "No can do.";
+
+  }
+  else if (distance >= 2000)
+  {
+    return "I will gladly take your thirty bucks.";
+  }
+  else
+  {
+    return "Thanks for riding";
+  }
+}
+
+function ternaryCheckCity(destination)
+{
+    let msg = destination === "NYC" ?  "Ok, sounds good." : "No go."
+    return msg;
+}
+
+function switchOnCharmFromTip(tip)
+{
+
+  switch (tip)
+  {
+    case "generous":
+    {
+      return "Thank you so much.";
+    }
+    case "not as generous":
+    {
+      return "Thank you.";
+    }
+    default:
+    {
+      return "Bye.";
+    }
+  }
+
+}
